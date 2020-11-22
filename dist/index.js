2c224a5eb5299163b93ee4afa0223aca66412fb7
afeiship <1290657123@qq.com>
2020-11-22 19:53:15 +0800

    Release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Priority queue for next.
  * homepage: https://github.com/afeiship/next-priority-queue
  * version: 1.0.0
- * date: 2020-11-22 19:51:56
+ * date: 2020-11-22 19:53:08
  * license: MIT
  */
 
